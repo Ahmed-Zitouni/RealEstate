486ae0bd727ae373aeb7751c8d57e52dd751eadd
Ahmed-Zitouni <ahmedz123645@gmail.com>
2019-11-17 13:08:26 -0500

    adding files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MNavbar from './Components/MNavbar';
+import UnitList from './Components/UnitList';
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <UnitList />
+    </React.Fragment>
+  );
+}
+
+export default App;
