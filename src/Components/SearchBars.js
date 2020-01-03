c7d0f192c5d053620ca9b67d449f8ff70b8f8f18
Ahmed-Zitouni <ahmedz123645@gmail.com>
2020-01-03 15:38:36 -0500

    optimizing site

--- src/Components/SearchBars.js
+++ src/Components/SearchBars.js
@@ -14,7 +14,12 @@ const SearchBars = () => {
     const [cityErr, setCityErr] = useState('1px solid #efefef');
     const [stateErr, setStateErr] = useState('1px solid #efefef');
 
-
+    const StatesUS = new Set([ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 
+    'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 
+    'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 
+    'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 
+    'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 
+    'WV', 'WI', 'WY'])
     let AddressBar = {
         fontSize: '90%',
         marginLeft: '20px',
@@ -32,10 +37,15 @@ const SearchBars = () => {
     let ValidText = "1px solid #efefef"
     let InvalidText = "1px solid #f05757"
 
+    const VaildState = (e) => {
+        let value = e.target.value
+        setStateErr(ValidText)
+        if(/^[a-z]+$/i.test(value) || value === "") setSta(value)
+    }
     let SearchHouse = (e) => {
         e.preventDefault();
         dispatch({type:'LOADING', loading: true})
-        if (address && city && sta) {
+        if (address.length > 0 && city.length > 0 && StatesUS.has(sta.toUpperCase)) {
 
         GetHouseInfo({addr: address.trim(), city: city.trim(), sta: sta.trim()})
             .then(data => { 
@@ -48,13 +58,14 @@ const SearchBars = () => {
                 }
             })
         } else {
+            dispatch({type:'LOADING', loading: false})
             if (!address) {
                 setAddresErr(InvalidText)
             }
             if (!city) {
                 setCityErr(InvalidText)
             }
-            if (!sta)
+            if (!StatesUS.has(sta.toUpperCase))
                 setStateErr(InvalidText)
             }
         }
@@ -64,7 +75,7 @@ const SearchBars = () => {
                 <form onSubmit={SearchHouse}>
                     <li style = {AddressErr}><FaSearch style = {SearchIcon}/><input style={AddressBar} type="text" placeholder="Address" maxLength="40" value={address} onChange={(e) => {setAddress(e.target.value); setAddresErr(ValidText)}}/></li>
                     <li style = {CityErr}><input type="text" placeholder="City" value={city} maxLength="20" onChange={(e) => {setCity(e.target.value); setCityErr(ValidText)}}/></li>
-                    <li style = {StateErr}><input type="text" placeholder="State" value={sta} maxLength="2" onChange={(e) => {setSta(e.target.value); setStateErr(ValidText)}} /></li>
+                    <li style = {StateErr}><input type="text" placeholder="State" value={sta} maxLength="2" onChange={(e) => VaildState(e)} /></li>
                     <input type="submit" value="Search"/>
                 </form>
             </div>
