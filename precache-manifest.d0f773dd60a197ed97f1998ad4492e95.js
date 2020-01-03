ebc3dd0e0b92f7850b4c5724af4b65528f9b1b7d
Ahmed-Zitouni <ahmedz123645@gmail.com>
2020-01-03 14:46:35 -0500

    Updates

---
+++ precache-manifest.d0f773dd60a197ed97f1998ad4492e95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4dc603bec33108d2a558ab34d58e39a",
+    "url": "/RealEstate/index.html"
+  },
+  {
+    "revision": "242ad23fd325d242281e",
+    "url": "/RealEstate/static/css/main.b3b3224c.chunk.css"
+  },
+  {
+    "revision": "d004a645d374b1035537",
+    "url": "/RealEstate/static/js/2.6787cb32.chunk.js"
+  },
+  {
+    "revision": "242ad23fd325d242281e",
+    "url": "/RealEstate/static/js/main.a343f893.chunk.js"
+  },
+  {
+    "revision": "181b9d0b4047029156a2",
+    "url": "/RealEstate/static/js/runtime-main.4345c340.js"
+  },
+  {
+    "revision": "e9d17c1eff74d8f7f7170d45cac4e6cf",
+    "url": "/RealEstate/static/media/MarkerIcon.e9d17c1e.svg"
+  }
+]);
